4600219d3471e3d564f4afe38ad98da9f96979ca
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2024-04-04 20:14:08 +0300

    commit 1.1

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/goit-react-hw-05-movies">
-    <React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
 // basename = '/goit-react-hw-05-movies';
