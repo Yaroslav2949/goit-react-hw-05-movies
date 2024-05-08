bc59acf70fe504e58580a20117a2a8d355c89f50
Yaroslav Dolynskyi <dolynskiy13@gmail.com>
2024-05-08 13:47:36 +0300

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'Components/App'
+import { App } from '../src/Components/App'
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
